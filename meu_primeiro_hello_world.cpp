b6b33fb6f68e7f41c61611bf429fe48a6ddbf70d
Everton Natã <37844708+evertonnatan@users.noreply.github.com>
2022-07-22 08:03:49 -0300

    Adicionando conceitos extraídos do livro do Deitel

--- meu_primeiro_hello_world.cpp
+++ meu_primeiro_hello_world.cpp
@@ -1,14 +1,20 @@
-#include <iostream>
+#include <iostream> // permite que o programa gere saída de dados na tela
 
 using namespace std;
 
-int main()
+int main() // a função main inicia a execução do programa
 
 {
     cout << "Hello, World" << std::endl;
     cout << "How are you?";
 
+    //Imprimindo uma linha de texto com múltiplas instruções.
+    
+    cout <<"Esta parte está na primeira linha, ";
+    cout <<"mas esta está na segunda!\n";
+
     return 0; 
 
 
 }
+
