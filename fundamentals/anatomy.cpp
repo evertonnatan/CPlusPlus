ce2a0f634f88684de1ee0544f48ddbf752350f04
Everton Natã <37844708+evertonnatan@users.noreply.github.com>
2022-08-20 16:54:50 -0300

    Fundamentos de C++

---
+++ fundamentals/anatomy.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+
+    // How is a book organized?
+    /*
+    Every line starting with # is a preprocessor directive
+    This is a directive that tells the preprocessor to include the contents of file
+    A code block is a grouping of one or more statements
+    A statements is a syntactic unit of an imperative programming language    
+    */
+
+    return 0;
+}
